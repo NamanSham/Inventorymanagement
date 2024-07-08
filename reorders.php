670bda86e02f6c524a34e04971086dd23c48c441
NamanSham <86928034+NamanSham@users.noreply.github.com>
2024-07-08 00:29:17 -0700

    Add files via upload

---
+++ reorders.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Reorders</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Reorders</h1>
+    <table>
+        <tr>
+            <th>Product ID</th>
+            <th>Product Name</th>
+            <th>Total Reordered</th>
+        </tr>
+        <?php require_once 'eorders.php';?>
+    </table>
+</body>
+</html>
